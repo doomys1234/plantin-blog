09f08447a5b444d30f55c55dae8fd1593a9d0684
Maksym <doomys1234@gmail.com>
2025-04-21 17:08:20 +0300

    added cors

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors';
+
+
 import * as AppRouter from './routes/AppRouter.js'
 const app = express()
-
+app.use(cors());
 AppRouter.createRouter(app);
 
 export default app;
