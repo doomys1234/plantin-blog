38eff0f2cadda50d882c2b823eb4c5450e12bc1d
Maksym <doomys1234@gmail.com>
2025-04-21 21:32:28 +0300

    some changes in index.js

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 
 import * as AppRouter from './routes/AppRouter.js'
 const app = express()
-app.use(cors());
-AppRouter.createRouter(app);
+// app.use(cors());
+// AppRouter.createRouter(app);
 
 export default app;
--- server/index.js
+++ server/index.js
@@ -1,17 +1,22 @@
-import * as http from 'node:http';
+// import * as http from 'node:http';
 import app from './app.js';
 
-const API_PORT = 3000;
+app.use("/", (req,res)=>{
+    res.send("hello world server");
+})
 
-(async function main() {
-    try {
-            const server = http.createServer({ maxHeaderSize: 1024 * 32 }, app);
-            server.listen(API_PORT, () => {
-                console.info(`The server is listening to port ${API_PORT}.`);
-            });
+app.listen(3000, console.log('Server is running on PORT 5000'))
+// const API_PORT = 3000;
 
-    } catch (error) {
-        console.error(error.message);
-        process.exit(1);
-    }
-}());
+// (async function main() {
+//     try {
+//             const server = http.createServer({ maxHeaderSize: 1024 * 32 }, app);
+//             server.listen(API_PORT, () => {
+//                 console.log(`The server is listening to port ${API_PORT}.`);
+//             });
+//
+//     } catch (error) {
+//         console.error(error.message);
+//         process.exit(1);
+//     }
+// }());
